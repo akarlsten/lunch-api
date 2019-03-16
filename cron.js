ba0e49e5c33102966bb64cc97aad12b6a66f1e74
Adam Karlsten <adamkarlsten@gmail.com>
2019-03-16 21:32:34 +0100

    added more restaurants, better logic for handling weekdays

--- cron.js
+++ cron.js
@@ -1,6 +1,16 @@
 // Runs the scraping once every 24 hrs and updates the db
-const { scrapeKolgas, scrapeSlagt, scrapeInThePink } = require('./lib/scraper/scraper')
+const {
+  writeDateToDb,
+  scrapeKolgas,
+  scrapeSlagt,
+  scrapeInThePink,
+  scrapeGlasklart,
+  scrapeMiaMarias
+} = require('./lib/scraper/scraper')
 
+writeDateToDb()
 scrapeKolgas()
 scrapeSlagt()
 scrapeInThePink()
+scrapeGlasklart()
+scrapeMiaMarias()
