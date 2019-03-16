443a12ffcf10a5b82480c374793b040219154a7b
Adam Karlsten <adamkarlsten@gmail.com>
2019-03-16 01:12:02 +0100

    made sure to return menuItems = [] during weekends when there are no daily lunches by checking for either too many entries (kolgas) or too few (slagthuset)

--- cron.js
+++ cron.js
@@ -1,6 +1,6 @@
 // Runs the scraping once every 24 hrs and updates the db
 const { scrapeKolgas, scrapeSlagt, scrapeInThePink } = require('./lib/scraper/scraper')
 
-// scrapeKolgas()
-// scrapeSlagt()
+scrapeKolgas()
+scrapeSlagt()
 scrapeInThePink()
