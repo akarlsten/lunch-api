d9b3443f9eecd2dac1cf3b878df7521ffd1d54e7
Adam Karlsten <adamkarlsten@gmail.com>
2019-03-20 23:30:41 +0100

    added another restaurant, refactored scraper functions to support async/await, added a Promise.all call to check when all scraping is done, added a simple express json server and node-schedule to run scrapers and update everything every day at 05:00.. phew

--- cron.js
+++ cron.js
@@ -1,16 +1,43 @@
 // Runs the scraping once every 24 hrs and updates the db
+const schedule = require('node-schedule')
+const path = require('path')
+const fs = require('fs')
+const moment = require('moment')
+
+const { db } = require(path.join(__dirname, '/lib/db/database'))
+const logStream = fs.createWriteStream(path.join(__dirname, 'log/log.txt'), { flags: 'a' })
 const {
   writeDateToDb,
   scrapeKolgas,
   scrapeSlagt,
   scrapeInThePink,
   scrapeGlasklart,
-  scrapeMiaMarias
+  scrapeMiaMarias,
+  scrapeNiagara
 } = require('./lib/scraper/scraper')
 
-writeDateToDb()
-scrapeKolgas()
-scrapeSlagt()
-scrapeInThePink()
-scrapeGlasklart()
-scrapeMiaMarias()
+const scrapePages = () => {
+  Promise.all([
+    //wait for all the scraping to finish and update the db
+    writeDateToDb(),
+    scrapeKolgas(),
+    scrapeSlagt(),
+    scrapeInThePink(),
+    scrapeGlasklart(),
+    scrapeMiaMarias(),
+    scrapeNiagara()
+  ]).then(() => {
+    logStream.write(`${moment().format('Y/M/D - HH:mm:ss')} - Database updated!\n`)
+    console.log(`Database updated! At: ${moment().format('Y/M/D - HH:mm:ss')}`)
+  })
+}
+
+// '0 5 * * *'
+
+const scheduledScrape = () => {
+  schedule.scheduleJob('0 5 * * *', () => {
+    scrapePages()
+  })
+}
+
+module.exports = { scheduledScrape }
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
-/* add stuff */
+const express = require('express')
 
-// TODO: Look at the gatsby template in ~/Dev/gatsby-template and figure out how to get it in here
+const { db } = require('./lib/db/database')
+const { scheduledScrape } = require('./cron')
+const app = express()
+
+const restaurantData = db.get('restaurants').value()
+
+app.get('/', async (req, res) => {
+  try {
+    res.send(restaurantData)
+  } catch (e) {
+    res.status(400).send(e)
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server up on port 3000.')
+  scheduledScrape()
+})
