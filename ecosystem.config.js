9671cec962f1a251de5571a9adbf9fae267d6675
Adam Karlsten <adamkarlsten@gmail.com>
2019-03-29 21:54:27 +0000

    moved the cron script into PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: 'lunch-api',
+    script: './server.js',
+    instances: 1,
+    exec_mode: "cluster",
+    autorestart: true,
+    watch: false,
+    ignore_watch: ["node_modules", "log", "db"]
+    max_memory_restart: "200M",
+    cron_restart: "0 6,7,8 * * *",
+  }]
+};
